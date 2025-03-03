0090c677503805548e138ef48a85e0edbdc4fdc5
Mahnoor Iqbal <mahnooriqbal6565@gmail.com>
2025-03-03 12:01:15 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using DemoAppReflection;
+
+namespace ReflectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Type type =typeof(SampleClass);
+
+            Console.WriteLine($"Class Name: {type.Name}\n");
+
+            //Displaying for Constructors
+            Console.WriteLine("Constructors");
+            ConstructorInfo[] constructors = type.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine(constructor);
+            }
+            Console.WriteLine();
+
+            //Display Properties
+            Console.WriteLine("Properties");
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.Name);
+            }
+            Console.WriteLine();
+
+            //Display metadata of utilizinhg the correct metadata the reflection method
+            Console.WriteLine("Methods");
+            MethodInfo[] methods = type.GetMethods();
+            foreach(MethodInfo method in methods)
+            {
+                Console.WriteLine(method.Name);
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+}
+
---
+++ SampleClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAppReflection
+{
+    public class SampleClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public SampleClass() { }
+        public SampleClass(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"ID: {Id}, Name: {Name}");
+        }
+    }
+}
